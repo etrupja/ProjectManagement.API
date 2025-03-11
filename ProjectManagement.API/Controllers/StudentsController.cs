d6a93fed5dfab9ea9fa17733eef47cea7d99342e
Ervis Trupja <ervistrupja@outlook.com>
2025-03-11 10:18:12 +0100

    intial api code

---
+++ ProjectManagement.API/Controllers/StudentsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet("GetStudents")]
+        public IActionResult GetStudents()
+        {
+            return Ok();
+        }
+
+        [HttpGet("GetStudentById")]
+        public IActionResult GetStudentById(int studentId)
+        {
+            //kontrollo nese ekziston ne db
+            //return Ok();
+
+            return NotFound();
+        }
+
+        [HttpPost("AddStudent")]
+        public IActionResult AddStudent()
+        {
+            return Ok();
+        }
+
+        [HttpPut("UpdateStudent")]
+        public IActionResult UpdateStudent()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("DeleteStudent")]
+        public IActionResult DeleteStudent()
+        {
+            return Ok();
+        }
+
+    }
+}
